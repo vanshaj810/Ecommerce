699e7168a733f37a0a3c5003026f45bc9c1ea903
Vanshaj Bhardwaj <79003511+vanshaj810@users.noreply.github.com>
2022-04-25 14:05:48 +0530

    changes done

--- README.md
+++ README.md
@@ -27,5 +27,5 @@ CLOUDINARY_NAME
 CLOUDINARY_API_KEY
 CLOUDINARY_API_SECRET
 _fill each filed with your info respectively_
-
+mongodb://localhost:27017/Ecommerce
 
--- frontend/src/component/layout/Footer/Footer.js
+++ frontend/src/component/layout/Footer/Footer.js
@@ -14,17 +14,17 @@ const Footer = () => {
       </div>
 
       <div className="midFooter">
-        <h1>ECOMMERCE.</h1>
+        <h1>Tradezilla.</h1>
         <p>High Quality is our first priority</p>
 
-        <p>Copyrights 2021 &copy; MeAbhiSingh</p>
+        <p>Copyrights 2021 &copy; MeVanshajBhardwaj</p>
       </div>
 
       <div className="rightFooter">
         <h4>Follow Us</h4>
-        <a href="http://instagram.com/meabhisingh">Instagram</a>
-        <a href="http://youtube.com/6packprogramemr">Youtube</a>
-        <a href="http://instagram.com/meabhisingh">Facebook</a>
+        <a href="http://instagram.com/vanshaj810">Instagram</a>
+        <a href="https://www.youtube.com/channel/UCBPeEoSAnuz2qrqomdknEpA">Youtube</a>
+        <a href="http://instagram.com/vanshaj810">Facebook</a>
       </div>
     </footer>
   );
